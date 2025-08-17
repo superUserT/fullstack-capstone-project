f11a05cf960c3e3ed6eb80569c966776afc6cedb
superUserT <rantshothabisomail@gmail.com>
2025-08-17 14:41:29 -0400

    updated routes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,36 +1,34 @@
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        const gifts = await collection.find({}).toArray();
 
         // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
     }
 });
 
-
-//small change
 router.get('/:id', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection("gifts");
 
         const id = req.params.id;
 
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        // Task 3: Find a specific gift by ID using the collection.findOne method and store in constant called gift
+        const gift = await collection.findOne({ _id: new ObjectId(id) });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -42,8 +40,6 @@ router.get('/:id', async (req, res) => {
         res.status(500).send('Error fetching gift');
     }
 });
-
-
 
 // Add a new gift
 router.post('/', async (req, res, next) => {
