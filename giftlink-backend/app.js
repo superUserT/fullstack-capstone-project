41790eea4f079c62f5e5122d1bf5a4cab592cecc
superUserT <rantshothabisomail@gmail.com>
2025-08-18 11:10:50 +0200

    updated register route

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pinoLogger = require("./logger");
-
 const connectToDatabase = require("./models/db");
 const { loadData } = require("./util/import-mongo/index");
 
@@ -21,22 +20,17 @@ connectToDatabase()
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require("./routes/giftRoutes");
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const authRoutes = require("./routes/authRoutes");
 const searchRoutes = require("./routes/searchRoutes");
-
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use("/api/gifts", giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use("/api/auth", authRoutes);
 app.use("/api/search", searchRoutes);
 
 // Global Error Handler
